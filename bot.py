0bd496246bf23b453ae3dfa9cd26c7e7295282ba
DaveGax <david7gaxiola@gmail.com>
2024-09-04 04:16:04 -0600

    Added bot event on_ready for / commands

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ intents = discord.Intents.all()
 intents.messages = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='/', intents=intents) #Prefix para usar los comandos '/'
+bot = commands.Bot(command_prefix='!', intents=intents) #Prefix para usar los comandos '!'
 
 CANAL_PERMITIDO = 1280153631743676448  #Id del canal donde se pueden usar comandos (TESTO)
 
@@ -23,23 +23,29 @@ def canal_requerido():
     return commands.check(predicate)
 
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.activity.Game(name='/info'),
+                              status=discord.Status.online)
+    await bot.tree.sync()
+    print(f'{bot.user.name} is logged in.')
+
+
 #Comando /info para ver la información del bot
-@bot.command(name='info')
-async def info(ctx):
+@bot.tree.command(name='info', description='See bot information')
+async def info(interaction: discord.Interaction):
     embed = discord.Embed(
         title = '__¡Discord bot in development!__',
         color = discord.Color.blue(),
         url = 'https://github.com/DaveGax/movies-discordbot'
     )
-    embed.add_field(name = '**Commands**:', value = '/popular <List of popular movies>\n/toprated <List of top rated movies>', inline=False)
+    embed.add_field(name = '**Commands**:', value = '!popular <List of popular movies>\n!toprated <List of top rated movies>', inline=False)
     embed.set_footer(text = 'In development by: DaveGax')
 
-
-    await ctx.message.delete()
-    await ctx.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
 
-#Comando /popular para ver las peliculas populares
+#Comando !popular para ver las peliculas populares
 @bot.command(name='popular')
 @canal_requerido() #Aplica el check al comando
 async def popular(ctx):
@@ -58,7 +64,7 @@ async def popular(ctx):
     await ctx.send(embed=embed)
 
 
-#Comando /toprated para ver las peliculas mejor valoradas
+#Comando !toprated para ver las peliculas mejor valoradas
 @bot.command(name='toprated')
 @canal_requerido() #Aplica el check al comando
 async def toprated(ctx):
